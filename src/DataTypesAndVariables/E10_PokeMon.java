839414a3fec047da6e98fa4344c9d54df578cde8
Bogomila Katsarska <90954520+BogomilaKatsarska@users.noreply.github.com>
2024-06-16 16:22:51 +0300

    Add files via upload

--- src/DataTypesAndVariables/E10_PokeMon.java
+++ src/DataTypesAndVariables/E10_PokeMon.java
@@ -1,4 +1,27 @@
 package DataTypesAndVariables;
 
+import java.util.Scanner;
+
 public class E10_PokeMon {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int power = Integer.parseInt(scanner.nextLine());
+        int distance = Integer.parseInt(scanner.nextLine());
+        int factor = Integer.parseInt(scanner.nextLine());
+        int startPower = power;
+
+        int countPokes = 0;
+
+        while (power >= distance && factor != 0){
+            power -= distance;
+            countPokes++;
+            if (power == startPower / 2){
+                if (factor != 0){
+                    power = power / factor;
+                }
+            }
+        }
+        System.out.println(power);
+        System.out.println(countPokes);
+    }
 }
